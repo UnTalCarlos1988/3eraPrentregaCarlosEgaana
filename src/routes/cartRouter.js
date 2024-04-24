703831bf7bb7c99ed81805dbb2f38b5d6c9e544f
carlos egana <carlos.egana.gutierrez@gmail.com>
2024-04-24 07:17:41 -0400

    first commit

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -15,7 +15,7 @@ cartRouter.post('/', async (req, res) => {
 cartRouter.get('/:cid', async (req, res) => {
     try {
         const cartId = req.params.cid
-        const cart = await cartModel.findById(cartId)
+        const cart = await cartModel.findOne({ _id: cartId })
         res.status(200).send(cart)
     } catch (error) {
         res.status(500).send(`Error interno del servidor al consultar carrito: ${error}`)
@@ -32,8 +32,8 @@ cartRouter.post('/:cid/:pid', async (req, res) => {
         const indice = cart.products.findIndex(product => product.id_prod == productId)
 
         if (indice != -1) {
-           
-            cart.products[indice].quantity = quantity 
+            //Consultar Stock para ver cantidades
+            cart.products[indice].quantity = quantity //5 + 5 = 10, asigno 10 a quantity
         } else {
             cart.products.push({ id_prod: productId, quantity: quantity })
         }
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,34 +1,26 @@
 import { Router } from "express";
-import productModel from "../models/product.js";
-
+import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js'
 const productsRouter = Router()
 
 productsRouter.get('/', async (req, res) => {
     try {
-        const { limit } = req.query
-        const prods = await productModel.find().lean()
-        let limite = parseInt(limit)
-        if (!limite)
-            limite = prods.length
-        const prodsLimit = prods.slice(0, limite)
-        res.status(200).render('templates/home', {
-            mostrarProductos: true,
-            productos: prodsLimit,
-            css: 'product.css'
-        })
+        const { limit, page, filter, ord } = req.query;
+        const prods = await getProducts(limit, page, filter, ord)
+
+        res.status(200).send(prods)
 
     } catch (error) {
         res.status(500).render('templates/error', {
             error: error,
-        })
+        });
     }
-})
+});
 
- 
+//: significa que es modificable (puede ser un 4 como un 10 como un 75)
 productsRouter.get('/:pid', async (req, res) => {
     try {
-        const idProducto = req.params.pid 
-        const prod = await productModel.findById(idProducto)
+        const idProducto = req.params.pid //Todo dato que se consulta desde un parametro es un string
+        const prod = await getProduct(idProducto)
         if (prod)
             res.status(200).send(prod)
         else
@@ -41,7 +33,7 @@ productsRouter.get('/:pid', async (req, res) => {
 productsRouter.post('/', async (req, res) => {
     try {
         const product = req.body
-        const mensaje = await productModel.create(product)
+        const mensaje = await createProduct(product)
         res.status(201).send(mensaje)
 
     } catch (error) {
@@ -52,8 +44,8 @@ productsRouter.post('/', async (req, res) => {
 productsRouter.put('/:pid', async (req, res) => {
     try {
         const idProducto = req.params.pid
-        const updateProduct = req.body
-        const prod = await productModel.findByIdAndUpdate(idProducto, updateProduct)
+        const upProduct = req.body
+        const prod = await updateProduct(idProducto, upProduct)
         res.status(200).send(prod)
 
     } catch (error) {
@@ -64,7 +56,7 @@ productsRouter.put('/:pid', async (req, res) => {
 productsRouter.delete('/:pid', async (req, res) => {
     try {
         const idProducto = req.params.pid
-        const mensaje = await productModel.findByIdAndDelete(idProducto)
+        const mensaje = await deleteProduct(idProducto)
         res.status(200).send(mensaje)
     } catch (error) {
         res.status(500).send(`Error interno del servidor al eliminar producto: ${error}`)
